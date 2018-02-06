56feb2667f09cab19ef387bcc7613129ed4ad896
Giuseppe Grosso <g.grosso@aeffegroup.biz>
2018-02-06 19:00:52 +0100

    aggiunta esempi

---
+++ Lezione1/JavaProject/src/biz/aeffegroup/lezione5/test/AddImageTest.java
@@ -0,0 +1,50 @@
+package biz.aeffegroup.lezione5.test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Font.FontFamily;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfGState;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+public class AddImageTest
+{
+	 
+    public static final String SRC = "c:/temp/hero.pdf";
+    public static final String DEST = "c:/temp/hero_watermarked.pdf";
+ 
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new AddImageTest().manipulatePdf(SRC, DEST);
+    }
+ 
+    public void manipulatePdf(String src, String dest) throws IOException, DocumentException {
+        PdfReader reader = new PdfReader(src);
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        PdfContentByte under = stamper.getUnderContent(1);
+        Font f = new Font(FontFamily.HELVETICA, 15);
+        Phrase p = new Phrase("This watermark is added UNDER the existing content", f);
+        ColumnText.showTextAligned(under, Element.ALIGN_CENTER, p, 297, 550, 0);
+        PdfContentByte over = stamper.getOverContent(1);
+        p = new Phrase("This watermark is added ON TOP OF the existing content", f);
+        ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 500, 0);
+        p = new Phrase("This TRANSPARENT watermark is added ON TOP OF the existing content", f);
+        over.saveState();
+        PdfGState gs1 = new PdfGState();
+        gs1.setFillOpacity(0.5f);
+        over.setGState(gs1);
+        ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, 297, 450, 0);
+        over.restoreState();
+        stamper.close();
+        reader.close();
+    }
+}
--- Lezione1/JavaProject/src/biz/aeffegroup/lezione5/test/MyFirstPdfTest.java
+++ Lezione1/JavaProject/src/biz/aeffegroup/lezione5/test/MyFirstPdfTest.java
@@ -55,4 +55,5 @@ public class MyFirstPdfTest
 		// step 5
 		document.close();
 	}
+	
 }
